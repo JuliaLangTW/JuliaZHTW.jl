60d59f014219357219ebe692c26fb90e6ce1c6ff
Daniel Chen <team6612@gmail.com>
2018-12-17 22:42:59 +0800

    Add some pages

--- docs/make.jl
+++ docs/make.jl
@@ -9,6 +9,8 @@ makedocs(
         "手冊" => [
             "入門 (Getting Started)" => "mannual/getting-started.md",
             "變數 (Variables)" => "mannual/variables.md",
+            "效能建議 (Performance Tips)" => "mannual/performance-tips.md",
+            "程式風格指引 (Style Guide)" => "mannual/style-guide.md",
         ],
     ],
     Documenter.HTML(
