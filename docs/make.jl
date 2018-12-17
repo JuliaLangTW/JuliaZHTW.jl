636f05325464594aa546b3aefa83decaf1fcce3b
Daniel Chen <team6612@gmail.com>
2018-12-17 23:20:47 +0800

    Fix typo of folder name

--- docs/make.jl
+++ docs/make.jl
@@ -7,10 +7,10 @@ makedocs(
     pages = [
         "首頁" => "index.md",
         "手冊" => [
-            "入門 (Getting Started)" => "mannual/getting-started.md",
-            "變數 (Variables)" => "mannual/variables.md",
-            "效能建議 (Performance Tips)" => "mannual/performance-tips.md",
-            "程式風格指引 (Style Guide)" => "mannual/style-guide.md",
+            "入門 (Getting Started)" => "manual/getting-started.md",
+            "變數 (Variables)" => "manual/variables.md",
+            "效能建議 (Performance Tips)" => "manual/performance-tips.md",
+            "程式風格指引 (Style Guide)" => "manual/style-guide.md",
         ],
     ],
     Documenter.HTML(
